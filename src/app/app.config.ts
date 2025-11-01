f6c5985eddb01090ca5fb08954e081974775156c
Ln/DsrkH9rp7DGx5QYFiQTseP4s0lsVFQrkKY0IIaRdGiXkfp4kaEXPjhEI= <manojsingh.n@ednectar.com-at-349674311220>
2025-11-02 02:29:18 +0530

    install ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,18 +6,16 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { globalErrorInterceptor } from './interceptor/error.interceptor';
+import { provideStore } from '@ngrx/store';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-     provideHttpClient(withInterceptors([globalErrorInterceptor])),
-     provideAnimations(),
-     importProvidersFrom(
-      ToastrModule.forRoot({
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([globalErrorInterceptor])),
+    provideAnimations(),
+    importProvidersFrom(ToastrModule.forRoot({
         positionClass: 'toast-top-right',
         preventDuplicates: true
-      })
-    )
-    ]
+    })), provideStore()]
 };
